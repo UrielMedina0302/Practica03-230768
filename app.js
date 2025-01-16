1c4e3f8e286dadab5395282a9b9c360088f853eb
Uriel-Abdallah-Medina-Torres <230768@utxicotepec.edu.mx>
2025-01-16 12:14:59 -0600

    Practica03-02
    Agregacion de login, update, status y logout

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(session({
 }));
 
 // Middleware para mostrar detalles de la sesión
-app.use((req, res, next)=> {
+/*app.use((req, res, next)=> {
     if(req.session) {
         if(!req.session.createdAt){
         req.session.createdAt = new Date(); //Asignamos la fecha de creación de la sesión
@@ -22,16 +22,73 @@ app.use((req, res, next)=> {
         req.session.lastAcces = new Date(); //Asignamos la ültima vez que se accedio a la sesion
     }
         next();
+});*/
+//Ruta para inicializar la sesión
+app.get('/login/:name', (req,res)=>{
+    const nm=req.params.name
+    if(!req.session.createdAt){
+        req.session.name=nm;
+        req.session.createdAt =new Date();
+        req.session.lastAcces =new Date();
+        
+        res.send(`La sesión ha sido iniciada. Bienvenido ${nm}`);
+        }else{
+        res.send('Ya existe una sesión');   
+        }
 });
 
+//Ruta para actualizar la fecha de la última consulta
+app.get('/update', (req,res)=>{
+if(req.session.createdAt){
+    req.session.lastAcces=new Date();
+    res.send('La fecha del último acceso ha sido actualizada');
+}else{
+    res.send('No hay sesión activa');
+}
+});
+
+//Ruta para obtener estado de la sesión 
+app.get('/status',(req,res)=>{
+    if(req.session.createdAt){
+        const now = new Date();
+        const started=new Date(req.session.createdAt);
+        const lastUpdate=new Date(req.session.lastAcces);
+        const name=req.session.name;
+        // calcular antiguedad de la sesion
+        const sessionAgeMs=now -started;
+        const hours= Math.floor(sessionAgeMs/(1000*60*60));
+        const minutes=Math.floor(sessionAgeMs%(1000*60*60)/(1000*60));
+        const seconds=Math.floor(sessionAgeMs%(1000*60)/1000);
+
+        //convertir las fechas al uso horario de CDMX
+        const createdAT_MX=moment(started).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+        const lastAcces_MX=moment(lastUpdate).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss');
+        res.json({
+            mensaje: 'Estado de la sesión',
+            SessionId:req.sessionID,
+            Usuario:name,
+            inicio:createdAT_MX,
+            ultimoAcceso:lastAcces_MX,
+            antiguedad: `${hours} horas, ${minutes} minutos y ${seconds} segundos`,
+            
+        });
+    }else{
+        res.send('No hay una sesión activa');
+    }
+});
+
+
 // Ruta parta mostrar la información de la sesion
-app.get('/session/:name', (req, res)=>{
+app.get('/session', (req, res)=>{
     if(req.session) {
         const SessionId = req.session.id;
-        const createdAt =req.session.createdAt;
-        const lastAcces = req.session.lastAcces;
-        const sessionDuration = (new Date() - createdAt) / 1000; // Duración de la sesión en segundos
-        const name1=req.params.name;
+        //const createdAt =req.session.createdAt;
+        /*const lastAcces = req.session.lastAcces;
+        const sessionDuration = (new Date() - createdAt) / 1000;*/ // Duración de la sesión en segundos
+        const createdAt= new Date(req.session.createdAt);
+        const lastAcces= new Date(req.session.lastAcces);
+        const sessionDuration= ((new Date()-createdAt)/1000).toFixed(2);
+        const name1=req.session.name;
         
         res.send(`
         <h1>Detalles de la sesión</h1>
@@ -42,16 +99,19 @@ app.get('/session/:name', (req, res)=>{
         <p><strong>Nombre de quien inicio sesion:</strong> ${name1}</p>
         `);
     }
-})
-
+});
 //Ruta para cerrar la sesion
-app.get('/logout',(req,res)=>{
-    res.session.destroy((err)=>{
+app.get('/logout', (req,res)=>{
+    if (req.session.createdAt){
+        req.session.destroy((err)=>{
         if(err){
-            return res.send('Error al cerrar sesion.');
+            return res.status(500).send('Error al cerrar sesion.');
         }
         res.send('<h1>Sesión cerrada exitosamente.</h1>');
-    });
+        })
+    }else{
+        res.send('No hay una sesión activa parta cerrar');
+    }
 });
 
 //Iniciar el servidor en el puerto 3000
